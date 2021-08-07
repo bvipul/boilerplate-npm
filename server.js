76c2a7b97b6ab01306f0c5a916aa3f96a202ec98
Vipul Basapati <basapativipulkumar@gmail.com>
2021-08-07 19:29:23 +0530

    public files

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 80
+
+app.use(express.static("public"))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`App is running...`)
 })
